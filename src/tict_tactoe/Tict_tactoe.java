88b04f03621fb6ca2e5119d113c405de27bda375
sumit <sumitagraharilock@gamil.com>
2024-07-26 09:00:24 +0530

    files added

---
+++ src/tict_tactoe/Tict_tactoe.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tict_tactoe;
+
+/**
+ *
+ * @author Sumit Agrahari
+ */
+public class Tict_tactoe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
